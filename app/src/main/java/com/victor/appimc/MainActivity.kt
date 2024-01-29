35bd562fca9a113e14a482edf6bdb486fe05a2e3
Victor-Valedev <victorvaledev@gmail.com>
2024-01-29 17:53:49 -0300

    app IMC

---
+++ app/src/main/java/com/victor/appimc/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.victor.appimc
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var btnCalcular: Button
+    lateinit var editPeso: EditText
+    lateinit var editAltura: EditText
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnCalcular = findViewById(R.id.btn_calcular)
+        editPeso = findViewById(R.id.edit_peso)
+        editAltura = findViewById(R.id.edit_altura)
+
+
+
+        btnCalcular.setOnClickListener {
+
+            val intent = Intent(this, ImcResultActivity::class.java)
+
+            val peso = editPeso.text.toString()
+            val altura = editAltura.text.toString()
+
+            if(peso.isNotEmpty() && altura.isNotEmpty()){
+                intent.putExtra("peso", peso.toDouble())
+                intent.putExtra("altura", altura.toDouble())
+
+            }
+
+
+
+            startActivity( intent )
+
+        }
+    }
+
+}
